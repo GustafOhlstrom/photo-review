62a5fe55dc3f354f658c24ac6669e7d1ec405196
gustafohlstrom <gustaf_ohlstrom@hotmail.com>
2021-01-10 14:39:21 +0100

    Added loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,8 @@
+import './Loader.scss'
+import React from 'react'
+
+const Loader = () => (
+	<div className="loader"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+)
+
+export default Loader
