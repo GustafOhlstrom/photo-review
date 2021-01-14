2e02d5a74b5523f2a47d12494c88df6296fdf086
gustafohlstrom <gustaf_ohlstrom@hotmail.com>
2021-01-14 20:04:10 +0100

    Added test for re-routing to home page when unauthorized

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render, act, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import App from './App'
+
+it('renders home page when not authorized', async () => {
+	const history = createMemoryHistory()
+	window.history.pushState({}, '', '/galleries')
+
+	await act(async () => {
+		render(
+			<Router history={history}>
+				<App />
+			</Router>
+		)
+	})
+
+	expect(screen.getByRole('heading', { name: 'Welcome to Photo Review' })).toBeInTheDocument()
+	expect(screen.getByRole('button', { name: 'Sign in' })).toBeInTheDocument()
+	expect(screen.getByRole('button', { name: 'Sign up' })).toBeInTheDocument()
+})
